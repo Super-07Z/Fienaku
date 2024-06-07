bb9a2b0c8d56769e82cbd9142bc019d749841b5b
Jose Enrique Limachi Garcia <lijoen@outlook.com>
2024-06-07 16:29:49 -0400

    Initial commit

---
+++ src/main/java/bancofie/com/bo/Fienaku/FienakuApplication.java
@@ -0,0 +1,13 @@
+package bancofie.com.bo.Fienaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FienakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FienakuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bancofie/com/bo/Fienaku/FienakuApplicationTests.java
@@ -0,0 +1,13 @@
+package bancofie.com.bo.Fienaku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FienakuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
