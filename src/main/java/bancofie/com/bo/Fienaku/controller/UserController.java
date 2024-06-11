a58642bd679e78118ac65fb6b68c630742e6f5f5
José Enrique Limachi García <lijoen@outlook.com>
2024-06-11 16:40:00 -0400

    CRUD USUARIO
    termine el crud de usuario no lo puedo probar aqui haci que lo hare en mi casa

--- src/main/java/bancofie/com/bo/Fienaku/controller/UserController.java
+++ src/main/java/bancofie/com/bo/Fienaku/controller/UserController.java
@@ -17,10 +17,11 @@ import bancofie.com.bo.Fienaku.upload.StorageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,7 +33,7 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final StorageService storageService;
-    
+
     @ApiResponses(value = {
         @ApiResponse(responseCode = "201", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = User.class))),
         @ApiResponse(responseCode = "400", description = "Bad Request", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ApiError.class))),
@@ -48,7 +49,7 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving users");
         }
     }
-    
+
     @Operation(summary = "List a one User")
     @PostMapping("/user/{id}")
     public User getOne(@PathVariable Long id) {
@@ -57,27 +58,46 @@ public class UserController {
         } catch (UserNotFoundException ex) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
         }
-    } 
+    }
 
-    @Operation(summary="Create user")
-    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE) 
-        public ResponseEntity<?> newUser(@RequestPart("user") User user, @RequestPart("file") MultipartFile file) {
+    @Operation(summary = "Create user")
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> newUser(@RequestPart("user") User user, @RequestPart("file") MultipartFile file) {
         String urlImagen = null;
 
         if (!file.isEmpty()) {
             String imagen = storageService.store(file);
             urlImagen = MvcUriComponentsBuilder.fromMethodName(StorageController.class, "serveFile", imagen, null).build().toUriString();
         }
-        
+
         User newUser = new User();
         newUser.setName(user.getName());
         newUser.setLastname(user.getLastname());
         newUser.setMail(user.getMail());
         newUser.setPassword(user.getPassword());
-        newUser.setImage(urlImagen);                        
+        newUser.setImage(urlImagen);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(newUser));
     }
-    
-    
+
+    @Operation(summary = "edit user")
+    @PostMapping("/edit/{id}")
+    public User editUser(@RequestBody User edit, @PathVariable Long id) {
+        return userRepository.findById(id).map(p -> {
+            p.setName(edit.getName());
+            p.setLastname(edit.getLastname());
+            p.setMail(edit.getMail());
+            p.setPassword(edit.getPassword());
+            return userRepository.save(p);
+        }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @Operation(summary = "delete user")
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+        userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
 }
