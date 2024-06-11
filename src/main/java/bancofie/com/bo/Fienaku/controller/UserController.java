ac66f7e301e54cf04aec4cfeff069f645446250e
José Enrique Limachi García <lijoen@outlook.com>
2024-06-11 17:01:35 -0400

    CRUD
    Usuarios

--- src/main/java/bancofie/com/bo/Fienaku/controller/UserController.java
+++ src/main/java/bancofie/com/bo/Fienaku/controller/UserController.java
@@ -1,6 +1,10 @@
 package bancofie.com.bo.Fienaku.controller;
 
-import java.util.List;
+import bancofie.com.bo.Fienaku.error.ApiError;
+import bancofie.com.bo.Fienaku.error.UserNotFoundException;
+import bancofie.com.bo.Fienaku.model.User;
+import bancofie.com.bo.Fienaku.model.UserRepository;
+import bancofie.com.bo.Fienaku.upload.StorageService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -8,38 +12,33 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import lombok.RequiredArgsConstructor;
 
-import bancofie.com.bo.Fienaku.model.User;
-import bancofie.com.bo.Fienaku.model.UserRepository;
-import bancofie.com.bo.Fienaku.error.ApiError;
-import bancofie.com.bo.Fienaku.error.UserNotFoundException;
-import bancofie.com.bo.Fienaku.upload.StorageService;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-
+    
     private final UserRepository userRepository;
     private final StorageService storageService;
-
+    
     @ApiResponses(value = {
         @ApiResponse(responseCode = "201", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = User.class))),
         @ApiResponse(responseCode = "400", description = "Bad Request", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ApiError.class))),
         @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ApiError.class)))
     })
-
+    
     @Operation(summary = "List Users")
     @PostMapping("/user")
     public List<User> getAll() {
@@ -49,8 +48,7 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving users");
         }
     }
-
-    @Operation(summary = "List a one User")
+    @Operation(summary = "List a user")
     @PostMapping("/user/{id}")
     public User getOne(@PathVariable Long id) {
         try {
@@ -59,8 +57,7 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
         }
     }
-
-    @Operation(summary = "Create user")
+    @Operation(summary = "Create User")
     @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> newUser(@RequestPart("user") User user, @RequestPart("file") MultipartFile file) {
         String urlImagen = null;
@@ -79,8 +76,7 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(newUser));
     }
-
-    @Operation(summary = "edit user")
+    @Operation(summary = "Edit User")
     @PostMapping("/edit/{id}")
     public User editUser(@RequestBody User edit, @PathVariable Long id) {
         return userRepository.findById(id).map(p -> {
@@ -91,13 +87,11 @@ public class UserController {
             return userRepository.save(p);
         }).orElseThrow(() -> new UserNotFoundException(id));
     }
-
-    @Operation(summary = "delete user")
+    @Operation(summary = "Delete User")
     @PostMapping("/delete/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
         userRepository.delete(user);
         return ResponseEntity.noContent().build();
     }
-
 }
