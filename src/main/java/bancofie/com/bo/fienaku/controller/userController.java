5f972c071ad72d910c4da3c8f5e40c90dce25aa7
José Enrique Limachi García <138630047+Super-07Z@users.noreply.github.com>
2024-06-24 12:21:39 -0400

    DTO usuario

--- src/main/java/bancofie/com/bo/fienaku/controller/userController.java
+++ src/main/java/bancofie/com/bo/fienaku/controller/userController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@RequestMapping("/fienaku")
+@RequestMapping("/user")
 @RestController
 
 public class userController {
